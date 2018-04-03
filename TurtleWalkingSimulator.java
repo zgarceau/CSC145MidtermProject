0fa98cfb7c02dbebc4bec6b33a61f51dba2527e9
zgarceau <38020034+zgarceau@users.noreply.github.com>
2018-04-03 13:47:38 -0500

    Add files via upload

---
+++ TurtleWalkingSimulator.java
@@ -0,0 +1,39 @@
+
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+//Name: Zachary Garceau & Blake Duschel
+//Course: CSC 145
+//Project: Midterm Project Option 2
+//File Name: TurtleWalkingSimulator.java
+
+public class TurtleWalkingSimulator extends Application
+{
+    private Button walk;
+    private TextField steps;
+    private ImageView image;
+    private Image turtle;
+    private Text finalWalkDistance;
+    private int stepCounter;
+    
+    public void start(Stage primaryStage)
+    {
+        
+    }
+    
+    public void processButtonPress()
+    {
+        
+    }
+
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }
+    
+}
