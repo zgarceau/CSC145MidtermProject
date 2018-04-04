9d67431140674ab26545d577e8d96203e36cbf6b
zgarceau <38020034+zgarceau@users.noreply.github.com>
2018-04-04 12:51:52 -0500

    Update TurtleWalkingSimulator.java

--- TurtleWalkingSimulator.java
+++ TurtleWalkingSimulator.java
@@ -1,9 +1,12 @@
-
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -23,7 +26,20 @@ public class TurtleWalkingSimulator extends Application
     
     public void start(Stage primaryStage)
     {
+        steps = new TextField();
+        steps.setMaxWidth(50);
         
+        walk = new Button("Walk");
+        //walk.setOnAction(this::processButtonPress);
+        
+        HBox guide = new HBox(steps, walk);
+        
+        Group root = new Group(guide);
+        Scene scene = new Scene(root, 700, 700, Color.BLACK);
+        
+        primaryStage.setTitle("Turtle Walking Simulator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     
     public void processButtonPress()
